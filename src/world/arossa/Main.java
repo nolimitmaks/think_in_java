f1917368f8f0a71612332d8268e26c5dfc8e9dc6
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-09-11 07:34:36 -0700

    everything_is_an_object[p61] -> ExerciseN2
    Exercise 2: Following the HelloDate.java example in this chapter, create a 'hello,
    world' program that simply displays that statement.

--- src/world/arossa/Main.java
+++ src/world/arossa/Main.java
@@ -1,14 +1,19 @@
 package world.arossa;
 
-import world.arossa.everything_is_an_object.ExerciseN1;
+//import world.arossa.everything_is_an_object.ExerciseN1;
+import world.arossa.everything_is_an_object.ExerciseN2;
 
 public class Main {
 
     public static void main(String[] args) {
 //        everything_is_an_object.ExerciseN1;
-        ExerciseN1 x = new ExerciseN1();
-        x.getInstanceFields();
+//        ExerciseN1 x = new ExerciseN1();
+//        x.getInstanceFields();
 
-        
+
+//        everything_is_an_object.ExerciseN2;
+        ExerciseN2 d = new ExerciseN2();
+        d.dateInfo();
+
     }
 }
---
+++ src/world/arossa/everything_is_an_object/ExerciseN2.java
@@ -0,0 +1,10 @@
+package world.arossa.everything_is_an_object;
+
+import java.util.Date;
+
+public class ExerciseN2 {
+    public void dateInfo () {
+        System.out.println("Date is ...");
+        System.out.println(new Date());
+    }
+}
